3f4e932c216fef6d02b51ce666762c23f81be488
Robert Malko <robmalko@gmail.com>
2014-04-20 11:47:37 -0400

    update api example to be more robust

--- techniques/convert_api_result_into_objects.rb
+++ techniques/convert_api_result_into_objects.rb
@@ -3,6 +3,10 @@ class Foo
   include RMExtensions::JsonKit
 
   def self.from_json(json)
+    if json.is_a?(Hash)
+      json = [json]
+    end
+    return [] if json.blank? || !json.is_a?(Array)
     json.map { |item| new(item) }
   end
 
